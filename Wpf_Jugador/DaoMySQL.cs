66dae8e144bc2778696a18e8ee3cc5224d8e0f86
Nicolás Hernández Jiménez <nicolimonar@gmail.com>
2017-06-13 22:37:53 +0200

    Clase jugador hecha, dao en camino

---
+++ Wpf_Jugador/DaoMySQL.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Collections.ObjectModel;
+
+namespace Wpf_Jugador
+{
+    public class DaoMySQL
+    {
+        MySqlConnection conexion;
+
+        public bool Conectar(string server, string usuario, string bada, string password)
+        {
+            string cadenaConexion = "server=" + server + ";database=" + bada + ";uid=" + usuario + ";pwd=" + password + ";";
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+                return true;
+            }
+            catch (MySqlException)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Desconectar()
+        {
+            try
+            {
+                if (conexion != null)
+                {
+                    conexion.Close();
+                    return conexion.State == System.Data.ConnectionState.Closed;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (MySqlException)
+            {
+                
+                throw;
+            }
+        }
+
+        public ObservableCollection<Jugador> Seleccionar(int? id)
+        {
+            try
+            {
+                ObservableCollection<Jugador> resultado = new ObservableCollection<Jugador>();
+                string orden;
+                if (conexion != null)
+                {
+                    if (id == null)
+                    {
+                        orden = "select id, nombre, apellido, altura, capitan, puesto, fecha_alta, salario, equipo from jugador;";
+                    }
+                    else
+                    {
+                        orden = "select id, nombre, apellido, altura, capitan, puesto, fecha_alta, salario, equipo from jugador where id = " + id + ";";
+                    }
+                    MySqlCommand cmd = new MySqlCommand(orden, conexion);
+                    MySqlDataReader lector = cmd.ExecuteReader();
+                    Jugador unJugador;
+                    while (lector.Read())
+                    {
+                        unJugador = new Jugador();
+                        unJugador.Id = int.Parse(lector["id"].ToString());
+                        unJugador.Nombre = lector["nombre"].ToString();
+                        unJugador.Apellido = lector["apellido"].ToString();
+                        unJugador.Altura = double.Parse(lector["altura"].ToString());
+                        unJugador.Capitan = int.Parse(lector["capitan"].ToString());
+                        unJugador.Puesto = int.Parse(lector["puesto"].ToString());
+                        unJugador.FechaAlta = lector["fecha_alta"].ToString();
+                        unJugador.Salario = double.Parse(lector["salario"].ToString());
+                        unJugador.Equipo = int.Parse(lector["equipo"].ToString());
+                        resultado.Add(unJugador);
+                    }                 
+                }
+                return resultado;
+            }
+            catch (MySqlException)
+            {
+                
+                throw;
+            }
+        }
+
+        public bool Insertar(Jugador unJugador)
+        {
+ 
+        }
+    }
+}
---
+++ Wpf_Jugador/Jugador.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Jugador
+{
+    public class Jugador
+    {
+        int? _id;
+        string _nombre;
+        string _apellido;
+        double? _altura;
+        int? _capitan;
+        int? _puesto;
+        string _fechaAlta;
+        double? _salario;
+        int _equipo;
+
+        public int? Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        
+        public double? Altura
+        {
+            get { return _altura; }
+            set { _altura = value; }
+        }
+        
+        public int? Capitan
+        {
+            get { return _capitan; }
+            set { _capitan = value; }
+        }
+        
+        public int? Puesto
+        {
+            get { return _puesto; }
+            set { _puesto = value; }
+        }
+        
+        public string FechaAlta
+        {
+            get { return _fechaAlta; }
+            set { _fechaAlta = value; }
+        }
+        
+        public double? Salario
+        {
+            get { return _salario; }
+            set { _salario = value; }
+        }
+        
+        public int Equipo
+        {
+            get { return _equipo; }
+            set { _equipo = value; }
+        }
+    }
+}
